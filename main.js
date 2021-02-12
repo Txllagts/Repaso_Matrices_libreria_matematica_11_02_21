28badde8cfa01be522d2ea7ad3031cb61d529870
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-02-12 12:53:30 -0500

    Repaso_Matrices_líbreeia_matematica_11_02_21

---
+++ main.js
@@ -0,0 +1,81 @@
+
+//declarar matrices perfectas
+
+//const cont = 10;
+//let array = new Array(cont);
+//for (let i = 0; i < cont; i++) {
+//    array[i] = new Array(cont).fill(null);
+//}
+//array[0][0] = "Jasbeith";
+//array[1][1] = "Prada";
+//array[2][2] = 16;
+//array[3][3] = true;
+//array[3][3] = "Calle 22 A".toUpperCase();
+
+
+
+//console,log(array);
+
+//Metodo de relleno con lista
+
+//let array = new Array(5).fill(()=>{return `Usuario no envio nada`});
+//array.fill("jasbeith",1,4);
+
+
+//console.log(array[0]);
+
+// matriz construida manual mente
+
+// const matriz = [
+//     "Miguel",
+//     23,
+//     true,
+//     ()=>{
+//         let data = 2 + 2;
+//         return `${data}`;
+//     },
+//     8%2,
+//     [
+//         [
+//             "Txllagts",
+//             "andrea"
+//         ]
+//     ]
+// ];
+
+// console.log(matriz[5][0][1]);
+
+
+//Libreria matematica
+//let resul = Math.sqrt(25);
+//console.log(resul);
+
+//let raiz_cubica = Math.cbrt(27);
+//console.log(raiz_cubica);
+
+//let numero_mayor = Math.max(1,5,100);
+//console-log(numero_mayor);
+
+//let numero_menor = Math.min(6,98,70,4);
+//console.log(numero_menor);
+
+
+//for (let i = 0; i < 10; i++) {
+//    let aleatorio = Math.random()*100;
+//    document.write(Math.trunc(aleatorio)+"<br>");
+//}
+
+// let aleatorio = Math.random()*100;
+// document.write(" Numero aleatorio <br>"+aleatorio.toString());
+// document.write("<br> Redondear despues de 5<br>"+Math.round(aleatorio));
+// document.write("<br> Redondear antesde de 5<br>"+Math.floor(aleatorio));
+// document.write("<br> Tomar el numero entero<br>"+Math.trunc(aleatorio));
+
+//console.log(Math.PI);
+//console.log(Math.SQRT1_2);
+//console.log(Math.SQRT2);
+//console.log(Math.E);
+//console.log(Math.LN2);
+//console.log(Math.LN10);
+//console.log(Math.LOG2E);
+//console.log(Math.LOG10E);
